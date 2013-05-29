f194874a0c034b97a1af68015f7c5418f3eabe93
Nelson Kelem <nelson@21waves.com>
2013-05-29 17:27:03 +0100

    Comment up configurations

--- lib/spree/epos_configuration.rb
+++ lib/spree/epos_configuration.rb
@@ -1,9 +1,13 @@
 module Spree
   module EposConfiguration < Preferences::Configuration
+    # I am not convinced this is the right approach because its set in stone.
+    # Maybe it should be left to admins to create terminals and assign them this configs
+    # That way we can create more terminals without relying on installing a copy of spree on each of them
+    #
     # Give the terminal a name
     preference :number, :string
 
-    # The location of the terminal. Which shop is it in?
+    # The location of the terminal. Which shop_branch is it in?
     preference :location, :string
 
     # The IP Address of the terminal
